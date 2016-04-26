aa6eb835d22fc90403bfd4e69e515c8dc6b4a68a
Christian Lindig <christian.lindig@citrix.com>
2016-04-26 11:26:41 +0100

    CP-16626 Added first sketch of negative VM test
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- tests/powercycle.ml
+++ tests/powercycle.ml
@@ -6,9 +6,12 @@ module C      = Rossa_config
 module X      = Rossa_xen
 module CMD    = Cmdliner
 module VM     = Xen_api_lwt_unix.VM
+module E      = Api_errors
 
 let return    = Xen_api_lwt_unix.return
 let (>>=)     = Xen_api_lwt_unix.(>>=)
+let sprintf   = Printf.sprintf
+
 
 exception Error of string
 let error fmt = Printf.kprintf (fun msg -> raise (Error msg)) fmt
@@ -17,7 +20,17 @@ let error fmt = Printf.kprintf (fun msg -> raise (Error msg)) fmt
  * implementation uses SSH to do this.  *)
 let xs_write server path value =
   let cmd = S.ssh server "xenstore write '%s' '%s'" path value in 
-    Yorick.(?| cmd)
+    Lwt.catch 
+      (fun ()     -> return Yorick.(?| cmd))
+      (function e -> Lwt.fail e)
+
+(** [xs_testing] writes a value into "control/testing" in Xen Store for
+ * [domid] on [server] *)
+let xs_testing server domid value =
+  xs_write 
+    server 
+    (sprintf "/local/domain/%Ld/control/testing" domid)
+    value
 
 (** find the named server in the inventory *)
 let find name servers =
@@ -28,15 +41,16 @@ let find name servers =
 let log fmt =
   Printf.kprintf (fun msg -> Lwt.return (print_endline @@ "# "^msg)) fmt
 
-(** [finf_template] finds a template by [name] *)
+(** [find_template] finds a template by [name] *)
 let find_template rpc session name =
   X.find_template rpc session name >>= function
     | Some t -> return t
     | None   -> X.fail "can't find template %s" name
 
-let powercycle template rpc session =
+let powercycle server template rpc session =
   let clone = "rossa-mirage-vm" in
   let meg32 = Rossa_util.meg 32 in
+  let crash = "now:crash" in
   find_template rpc session template >>= fun (t,_) ->
   log "found template %s" template >>= fun () ->
   VM.clone rpc session t clone >>= fun vm ->
@@ -51,17 +65,31 @@ let powercycle template rpc session =
     ~dynamic_max:meg32 >>= fun () ->
   log "cloned '%s' to '%s'" template clone >>= fun () ->
   VM.start rpc session vm false false >>= fun () -> 
-  log "VM started" >>= fun () ->
-  (* VM.suspend rpc session vm >>= fun () ->
-   * log "VM suspended" >>= fun () ->
-   * VM.resume rpc session vm true true >>= fun () ->
-   * log "VM resumed" >>= fun () ->
-   *)
-  VM.clean_shutdown rpc session vm >>= fun () ->
-  log "VM shut down" >>= fun () ->
-  VM.destroy rpc session vm >>= fun () ->
-  log "VM destroyed" >>= fun () ->
-  Lwt.return ()
+    Lwt.catch 
+      (fun () ->
+        log "VM started" >>= fun () ->
+        VM.get_domid rpc session vm >>= fun domid ->
+        log "VM domid is %Ld" domid >>= fun () ->
+        (* VM.suspend rpc session vm >>= fun () ->
+         * log "VM suspended" >>= fun () ->
+         * VM.resume rpc session vm true true >>= fun () ->
+         * log "VM resumed" >>= fun () ->
+         *)
+        log "sending message %s to %Ld" crash domid >>= fun () ->
+        xs_testing server domid crash >>= fun () ->
+        Lwt_unix.sleep 5.0 >>= fun () ->
+        VM.clean_shutdown rpc session vm >>= fun () ->
+        log "VM shut down" >>= fun () ->
+        VM.destroy rpc session vm >>= fun () ->
+        log "VM destroyed" >>= fun () ->
+        Lwt.return ())
+    (function
+      | E.Server_error("VM_BAD_POWER_STATE",_) ->
+        log "caught exception (as expected) .. cleaning up" >>= fun () ->
+        VM.destroy rpc session vm >>= fun () ->
+        log "VM destroyed" >>= fun () ->
+        Lwt.return ()
+      | e -> Lwt.fail(e))
 
 (* [main] is the heart of this test *)
 let main servers_json config_json  = 
@@ -72,7 +100,7 @@ let main servers_json config_json  =
   let server    = find hostname servers in
   let api       = S.api server in
   let root      = S.root server in
-    Lwt_main.run (X.with_session api root (powercycle template))
+    Lwt_main.run (X.with_session api root (powercycle server template))
 
 let servers =
   let doc = "JSON file describing Xen Servers available for testing." in
